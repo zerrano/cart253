abdc075f6d42f1802de12debc45ff8fcfdc6f945
Timothy <tm.serrano@icloud.com>
2019-09-17 19:59:31 -0400

    added text and text properties

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -5,6 +5,8 @@
 // Draws a moving square and circle that intersect
 // in the middle of the canvas.
 
+
+
 // The current position and size of the circle
 let circleX;
 let circleY;
@@ -15,7 +17,19 @@ let squareX;
 let squareY;
 let squareSize = 100;
 
+//attemptted to add text
+let tim = "tim is awesome";
 
+
+//**new shape
+let rectangleX;
+let rectangleY;
+let rectangleSize = 100;
+
+//**new shape part 2
+let circle2X;
+let circle2Y;
+let circle2Size = 100;
 // preload()
 //
 // Nothing here
@@ -32,12 +46,14 @@ function preload() {
 function setup() {
   // Create our canvas
   createCanvas(640,640);
-
   // Start the circle off screen to the bottom left
   // We divide the size by two because we're drawing from the center
   circleX = -circleSize/2;
   circleY = height + circleSize/2;
 
+  //rectangle will start on the left side of the screen
+  rectangleX = rectangleSize*2;
+  rectangleY = rectangleSize*2;
   // Start the square off screen to the bottom right
   // We divide the size by two because we're drawing from the center
   squareX = width + squareSize/2;
@@ -57,7 +73,10 @@ function setup() {
 
 function draw() {
   // We don't fill the background so we get a drawing effect
-
+  //text properties for tim
+  fill(0,0,255);
+  textSize(30);
+  text(tim, width/2, height/2);
   // Move circle up and to the right
   circleX += 1;
   circleY -= 1;
@@ -65,6 +84,13 @@ function draw() {
   fill(255,0,0,10);
   // Display the circle
   ellipse(circleX,circleY,circleSize,circleSize);
+
+  rectangleX += 2;
+  rectangleY -= 2;
+  // Make the circle transparent red
+  fill(200,55,5,10);
+  // Display the circle
+  rect(rectangleX+70,rectangleY+70,rectangleSize+40,rectangleSize+40);
 
   // Move square up and to the left
   squareX -= 1;

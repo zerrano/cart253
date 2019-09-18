4bdc356220173b9fd67ee1ddafe0c01bf68bc20b
Timothy <tm.serrano@icloud.com>
2019-09-18 14:08:04 -0400

    got both shrek images to finally work

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -16,23 +16,21 @@ let squareX;
 let squareY;
 let squareSize = 100;
 
-// Shrek's attributes
-let shrekpng;
-let shrekXPosition = 0;
-let shrekYPosition = 0;
-let shrekWidth = 100;
-let shrekHeight = 100;
+// Shrek's attributes, he'll be flying from the bottom left of the screen
+let shrek;
+let shrekX;
+let shrekY;
+let shrekSize = 100;
 
+//adding another shrek image.... just because. he'll be coming from the bottom right
+let shrek2;
+let shrek2X;
+let shrek2Y;
+let shrek2Size = 100;
 
-//**new shape
-let rectangleX;
-let rectangleY;
-let rectangleSize = 100;
+//add some saucy text
+let tim="tim is sexy";
 
-//**new shape part 2
-let circle2X;
-let circle2Y;
-let circle2Size = 100;
 // preload()
 //
 
@@ -41,7 +39,8 @@ let circle2Size = 100;
 // pre-loading shrek
 
 function preload() {
-  shrek = loadImage('./assets/images/shrek.png');
+  shrek = loadImage('/assets/images/shrek.png');
+  shrek2 = loadImage('/assets/images/shrek.jpg');
 }
 
 
@@ -52,60 +51,40 @@ function preload() {
 function setup() {
   // Create our canvas
   createCanvas(640,640);
-  image(shrek, 0, 0)
-  //shrek will spawn at mouse cursor location
-  displayImgAtMouse(shrek, 120, 120);
-  // Start the circle off screen to the bottom left
-  // We divide the size by two because we're drawing from the center
-  circleX = -circleSize/2;
-  circleY = height + circleSize/2;
 
-  //rectangle will start on the left side of the screen
-  rectangleX = rectangleSize*2;
-  rectangleY = rectangleSize*2;
-  // Start the square off screen to the bottom right
-  // We divide the size by two because we're drawing from the center
-  squareX = width + squareSize/2;
-  squareY = height + squareSize/2;
+  //loading our sexy shrek image at the bottom left, divide by two since we'll draw from the center
+  shrekX = -shrekSize/2;
+  shrekY = height + shrekSize/2;
 
-  // We'll draw rectangles from the center
-  rectMode(CENTER);
-  // We won't have a stroke in this
-  noStroke();
+  //loading our sexy shrek image at the bottom right, divide by two since we'll draw from the center
+  shrek2X = shrek2Size/2+600;
+  shrek2Y = height + shrek2Size/2;
+
+  //images start from the center
+  imageMode(CENTER);
+
 }
 
 
 // draw()
-//
-// Change the circle and square's positions so they move
-// Draw the circle and square on screen
 
 function draw() {
   // We don't fill the background so we get a drawing effect
-  //text properties for tim
+  //text properties for tim text, it will always be on cursor
   fill(0,0,255);
   textSize(30);
-  text(tim, width/2, height/2);
-  // Move circle up and to the right
-  circleX += 1;
-  circleY -= 1;
-  // Make the circle transparent red
-  fill(255,0,0,10);
-  // Display the circle
-  ellipse(circleX,circleY,circleSize,circleSize);
+  text(tim, mouseX, mouseY);
+  // adding a background to eliminate trailing
+  background (100, 300, 100, 100);
 
-  rectangleX += 2;
-  rectangleY -= 2;
-  // Make the circle transparent red
-  fill(200,55,5,10);
-  // Display the circle
-  rect(rectangleX+70,rectangleY+70,rectangleSize+40,rectangleSize+40);
+  //shrek will be gracefully moving to the top right
+  shrekX += 0.7;
+  shrekY -= 0.7;
+  image (shrek, shrekX, shrekY, shrekSize, shrekSize);
 
-  // Move square up and to the left
-  squareX -= 1;
-  squareY -= 1;
-  // Make the square transparent blue
-  fill(0,0,255,10);
-  // Display the square
-  rect(squareX,squareY,squareSize,squareSize);
+  //shrek will be gracefully flying to the top left
+  shrek2X -= 0.7;
+  shrek2Y -= 0.7;
+  image (shrek2, shrek2X, shrek2Y, shrek2Size, shrek2Size);
+
 }

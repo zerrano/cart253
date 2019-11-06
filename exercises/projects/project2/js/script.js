ce7ba9be39258d5351015b473a09d1d6d6ff14cd
Timothy <tm.serrano@icloud.com>
2019-11-06 13:22:56 -0500

    created our player 2: the cat

--- exercises/projects/project2/js/script.js
+++ exercises/projects/project2/js/script.js
@@ -5,8 +5,9 @@
 // The predator chases the prey using the arrow keys and consumes them.
 // The predator loses health over time, so must keep eating to survive.
 
-// Our predator
+// Our predators
 let tiger;
+let cat;
 
 // The three prey
 let antelope;
@@ -15,6 +16,7 @@ let bee;
 
 //images
 let tigerImg;
+let catImg;
 let antelopeImg;
 let zebraImg;
 let beeImg;
@@ -25,14 +27,16 @@ let beeImg;
 // Creates objects for the predator and three prey
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, 100, 5, 40, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, tigerImg, SHIFT);
-  antelope = new Prey(100, 100, 10, 50, antelopeImg);
+  tiger = new Predator(100, 100, 5, 70, UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW, tigerImg, SHIFT);
+  cat = new Predator(150, 150, 7, 50, 87, 83, 65, 68, catImg, 81);
+  antelope = new Prey(100, 100, 10, 80, antelopeImg);
   zebra = new Prey(100, 100, 8, 60, zebraImg);
-  bee = new Prey(100, 100, 20, 10, beeImg);
+  bee = new Prey(100, 100, 20, 30, beeImg);
 }
 
 function preload() {
   tigerImg = loadImage("assets/images/tiger.png");
+  catImg = loadImage("assets/images/cat.png");
   antelopeImg = loadImage("assets/images/antelope.png");
   zebraImg = loadImage("assets/images/zebra.png");
   beeImg = loadImage("assets/images/bee.png");
@@ -44,22 +48,29 @@ function draw() {
   // Clear the background to black
   background(0);
 
-  // Handle input for the tiger
+  // Handle input for the predators
   tiger.handleInput();
+  cat.handleInput();
 
   // Move all the "animals"
   tiger.move();
+  cat.move();
   antelope.move();
   zebra.move();
   bee.move();
 
-  // Handle the tiger eating any of the prey
+  // Handle the predators eating any of the prey
   tiger.handleEating(antelope);
   tiger.handleEating(zebra);
   tiger.handleEating(bee);
 
+  cat.handleEating(antelope);
+  cat.handleEating(zebra);
+  cat.handleEating(bee);
+
   // Display all the "animals"
   tiger.display();
+  cat.display();
   antelope.display();
   zebra.display();
   bee.display();
